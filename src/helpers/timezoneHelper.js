24fa706e074ac507283bcd7e2b8b3a3d2bb37a6f
abdullateef97 <adeniranyusufabdullateef@gmail.com>
2019-11-09 22:47:50 +0100

    added capability to get timezone from lat long

--- src/helpers/timezoneHelper.js
+++ src/helpers/timezoneHelper.js
@@ -1,11 +1,16 @@
 const city_tz = require('city-timezones')
 const moment_tz = require('moment-timezone')
+const geo_tz = require('geo-tz')
 
 
 exports.getTimeZoneFromCityName = city_name => {
   const tz_response = city_tz.lookupViaCity(city_name)
   console.log({tz_response})
   return tz_response && tz_response.length > 0 ? tz_response[0].timezone : null
+}
+
+exports.getTimezoneFromLatLong = (lat, long) => {
+  return geo_tz(lat, long)[0]
 }
 
 
